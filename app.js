f34cb16682776bac8040254565ad12222a2089f3
ThatOneTqnk <chattanoogamuckers@gmail.com>
2018-03-05 21:03:03 -0600

    Added style to the home page

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var request = require('request');
+var app = express();
+var path = require('path');
+var PORT = process.env.PORT || 5000;
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('pages/index');
+});
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
---
+++ public/js/typeload.js
@@ -0,0 +1,10 @@
+$(document).ready(() => {
+    $("span.typed").typed({
+        strings: ["Efficiency", "Easy", "Manages time"],
+        typeSpeed: 50,
+        backSpeed: 30,
+        backDelay: 700,
+        loop: true
+    });
+
+});

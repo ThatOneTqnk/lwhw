4e6c297f2c97af9d59ecc69cf1db803f449a9f25
BuildTools <unconfigured@null.spigotmc.org>
2018-10-02 18:28:53 -0500

    yee

--- public/js/hws.js
+++ public/js/hws.js
@@ -4,7 +4,7 @@ $(document).ready(() => {
         console.log(data);
         let totalstr = '';
         data.assignments.forEach((val) => {
-            totalstr += `<div class="card col-md-6"><div class="card-body"><strong>Assignment: </strong><p>${val.title}</p></div></div><br>`
+            totalstr += `<div class="card col-md-6"><div class="card-body"><strong>Homework: </strong><p>${val.title}</p></div></div><br>`
         });
         document.getElementById('load').style.display = 'none';
         document.getElementById('hws').innerHTML = totalstr;
